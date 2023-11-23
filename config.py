4f578a514cd8f92e5b0e4791b910f247b2e5857f
Hahabroha <146943572+Hahabroha@users.noreply.github.com>
2023-11-23 09:03:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,22 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "21748181")
+API_ID = os.environ.get("API_ID", "25435105")
 
-API_HASH = os.environ.get("API_HASH", "b1d962414e186e0778911f3183feac33")
+API_HASH = os.environ.get("API_HASH", "011126265844f2d7cc7dc1a024f6bc78")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6371916295:AAHCF7nnlt7lBCfGucEzhZ4adLkvIEXG1lg") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6661938398:AAFNwFybrVOaxSxkGAkeOPjLbZZJ86HaGN0") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 
-DB_NAME = os.environ.get("DB_NAME","User_Data")     
+DB_NAME = os.environ.get("DB_NAME","cluster0")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://rajbot:pass@cluster0.kgciulo.mongodb.net/?retryWrites=true&w=majority")
- 
+DB_URL = os.environ.get("DB_URL","mongodb+srv://BIGBOSS:BIGBOSS@cluster0.ii3gmvr.mongodb.net/?retryWrites=true&w=majority")
+
 FLOOD = int(os.environ.get("FLOOD", "100"))
 
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/2acf37dd041dc8cae6fc2.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5651594253').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6459102722').split()]
 
 PORT = os.environ.get("PORT", "8080")
